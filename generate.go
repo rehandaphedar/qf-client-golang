4e9bd6ce5b85151658e1382b5a193659ce7e79ce
Rehan Daphedar <mail@rehandaphedar.com>
2025-09-01 03:04:38 +0530

    rename user-related-apis to user-apis

--- generate.go
+++ generate.go
@@ -2,4 +2,4 @@ package main
 
 //go:generate oapi-codegen -config config.yaml -package content -o pkg/content/main.gen.go schemas/content-apis/v4.json
 //go:generate oapi-codegen -config config.yaml -package oauth2 -o pkg/oauth2/main.gen.go schemas/oauth2-apis/v1.json
-//go:generate oapi-codegen -config config.yaml -package user -o pkg/user/main.gen.go schemas/user-related-apis/v1.json
+//go:generate oapi-codegen -config config.yaml -package user -o pkg/user/main.gen.go schemas/user-apis/v1.json
